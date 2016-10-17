2e72a9d23066bb30bd6a87ef57d0dcb881cc414b
diootomo <thunpisith.ittipoomtaksa@gmail.com>
2016-10-17 18:07:41 +0700

    SETUP : Firebase
    https://diootomo-ad753.firebaseio.com/

---
+++ DLC/firebase.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.5.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCBTISYSiWJcmHY09WvjBN84PtnL78pq_I",
+    authDomain: "diootomo-ad753.firebaseapp.com",
+    databaseURL: "https://diootomo-ad753.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "145181770247"
+  };
+  firebase.initializeApp(config);
+</script>
