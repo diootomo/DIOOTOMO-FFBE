733616afd1ffca0a1e464bc426d1030f07d6bc56
diootomo <thunpisith.ittipoomtaksa@gmail.com>
2016-10-18 16:32:10 +0700

    Created : Hosting Firebase
    URL: diootomo-ad753.firebaseapp.com

--- public/DLC/firebase.js
+++ public/DLC/firebase.js
@@ -1,3 +1,4 @@
+
 <script src="https://www.gstatic.com/firebasejs/3.5.0/firebase.js"></script>
 <script>
   // Initialize Firebase
@@ -5,7 +6,7 @@
     apiKey: "AIzaSyCBTISYSiWJcmHY09WvjBN84PtnL78pq_I",
     authDomain: "diootomo-ad753.firebaseapp.com",
     databaseURL: "https://diootomo-ad753.firebaseio.com",
-    storageBucket: "",
+    storageBucket: "diootomo-ad753.appspot.com",
     messagingSenderId: "145181770247"
   };
   firebase.initializeApp(config);
