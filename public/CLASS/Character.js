0f3f0f4e4f47aeb79aba76a1326274c62892581b
diootomo <thunpisith.ittipoomtaksa@gmail.com>
2016-10-18 17:43:16 +0700

    Created Extend : head.html
    All Head in this, Except link

--- public/CLASS/Character.js
+++ public/CLASS/Character.js
@@ -1,5 +1,1 @@
-DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-DatabaseReference mUsersRef = mRootRef.child("users");
-DatabaseReference mMessagesRef = mRootRef.child("messages");
-FriendlyMessage friendlyMessage = new FriendlyMessage("Hello World!", "Jirawatee");
-mMessageRef.push().setValue(friendlyMessage);
+'use strict';
