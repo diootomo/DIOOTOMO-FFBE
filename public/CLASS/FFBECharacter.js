221f84e3f6b802e7e870b32cc24a09aa83ba1a76
diootomo <thunpisith.ittipoomtaksa@gmail.com>
2016-10-21 12:20:41 +0700

    GOAL : insert data into NODE Firebase with jQuery
    not use class

--- public/CLASS/FFBECharacter.js
+++ public/CLASS/FFBECharacter.js
@@ -1,8 +1,24 @@
-
-var database = firebase.database();
-
-function writeUserData(userId, name) {
-    firebase.database().ref('FFBECharacters/' + userId).set({
-        name: name
+$(document).ready(function () {
+    console.log("this work in FFBE function");
+    var database = firebase.database();
+   
+    $("#btnSubmitAddChar").click(function () {
+        console.log("this work in writeFFBE");
+        
+        console.log(""+$("#charName").val());
+        console.log(+$("#charHP").val());
+        
+        firebase.database().ref('FFBECharacters/' + ""+$("#charName").val()).set({
+            charName: ""+$("#charName").val()
+            , charHP: +$("#charHP").val()
+            , charMP: +$("#charMP").val()
+            , charATK: +$("#charATK").val()
+            , charDEF: +$("#charDEF").val()
+            , charMAG: +$("#charMAG").val()
+            , charSPR: +$("#charSPR").val()
+            , charHITS: +$("#charHITS").val()
+            , charDropCHK: +$("#charDropCHK").val()
+        });
+        
     });
-}
+});
